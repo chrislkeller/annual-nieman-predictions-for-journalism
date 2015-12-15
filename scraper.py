7e3d16eea97de10fda96b6b1dc2c197ea73a7b22
Chris Keller <christopherlawrencekeller@gmail.com>
2015-12-15 15:48:51 -0800

    updates scraper to pull 2016 predictions

--- scraper.py
+++ scraper.py
@@ -2,7 +2,14 @@
 
 import re
 import requests
+import logging
 from BeautifulSoup import BeautifulSoup
+
+logger = logging.getLogger("root")
+logging.basicConfig(
+    format = "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s",
+    level=logging.DEBUG
+)
 
 class Scraper(object):
     """ # """
@@ -11,39 +18,46 @@ class Scraper(object):
         "User-agent": ""
     }
 
-    live_html = "http://www.niemanlab.org/collection/predictions-2015/"
+    # year = 2015
+    year = 2016
+
+    # live_html = "http://www.niemanlab.org/collection/predictions-2015/"
+    live_html = "http://www.niemanlab.org/collection/predictions-2016/"
 
     def handle(self, *args, **kwargs):
         """ # """
         print "Running Nieman Prediction Scraper"
-        self.get_list_of_content_urls()
-        self.get_each_article()
+        this_article_list = self.get_list_of_content_urls()
+        self.get_each_article(this_article_list)
 
     def get_list_of_content_urls(self):
         """ # """
-
         r = requests.get(self.live_html, headers=self.headers)
         soup = BeautifulSoup(r.text)
-        item_list = soup.findAll("div", {"class": "predix-loop-article"})
-        self.article_list = []
+        soup = soup.find("div", {"class": "predix2016-mostrecentorder"})
+        item_list = soup.findAll("a")
+        this_article_list = []
         for item in item_list:
-            article_author = item.find("span", {"class": "newfront-other-recent-author"}).text.encode("utf-8")
-            article_url = item.findAll("a")[1]["href"]
-            article_headline = item.findAll("a")[1].text.encode('utf-8')
+            article_author = item.find("div", {"class": "newfront-other-recent-author"}).text.encode("utf-8")
+            article_url = item["href"]
+            article_headline = item.find("div", {"class": "predix-loop-headline"}).text.encode("utf-8")
             file_name = re.sub(r'\W+', ' ', article_headline)
             file_name = file_name.lower().replace(" ", "-")
-            self.article_list.append({
+            this_article_list.append({
                 "article_author": article_author,
                 "article_url": article_url,
                 "article_headline": article_headline,
                 "file_name": file_name
             })
+        logger.debug(this_article_list)
+        return this_article_list
 
-    def get_each_article(self):
+    def get_each_article(self, article_list):
         """ # """
-        html_file= open("index.html", "w")
-        html_file.write("<h1>Nieman Lab Predictions for Journalism 2015</h1>")
-        for obj in self.article_list:
+        filename = "_%d_predictions_index.html" % (self.year)
+        html_file= open(filename, "w")
+        html_file.write("<h1>Nieman Lab Predictions for Journalism %d</h1>" % (self.year))
+        for obj in article_list:
             print "Scraping %s" % (obj["article_headline"])
             r = requests.get(obj["article_url"], headers=self.headers)
             soup = BeautifulSoup(r.text)
